87e8473f8239d1b966c3fdbc1cf233b9cff9637e
Tim Lu <tim.lu@linecrop.com>
2023-03-22 16:43:33 +0800

    fix swiper

--- src/views/HomePage/index.vue
+++ src/views/HomePage/index.vue
@@ -6,8 +6,7 @@
           <img ref="imgs" :src="src" @load="loadImage(index)" />
         </div>
       </div>
-      <div class="swiper-button-prev"></div>
-      <div class="swiper-button-next"></div>
+      <div class="swiper-pagination"></div>
     </div>
   </div>
 </template>
@@ -18,7 +17,7 @@ import Swiper from 'swiper/swiper-bundle'
 import 'swiper/swiper-bundle.css'
 
 const loadImgs = require.context('@/assets/imgs', true, /.*.jpg$/)
-const imgsSrc = loadImgs.keys().map(filename => {
+const imgsSrc = loadImgs.keys().map((filename) => {
   return require(`@/assets/imgs${filename.slice(1)}`)
 })
 
@@ -26,9 +25,11 @@ const SWIPER_CONTAINER = 'swiper-container'
 
 const swiperConfig = {
   loop: true,
+  spaceBetween: 100,
   speed: 400,
   autoplay: {
-    delay: 1000,
+    delay: 5000,
+    disableOnInteraction: true,
   },
   effect: 'coverflow',
   grabCursor: true,
@@ -36,45 +37,55 @@ const swiperConfig = {
   slidesPerView: 'auto',
   coverflowEffect: {
     rotate: 0,
-    stretch: 80,
-    depth: 200,
+    stretch: 0,
+    depth: 300,
     modifier: 1,
     slideShadows: false,
   },
 
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+  pagination: {
+    el: '.swiper-pagination',
   },
 }
 
 export default {
   name: 'HomePage',
-  data () {
+  data() {
     return {
       imgsSrc,
       SWIPER_CONTAINER,
       colors: Array.from({ length: imgsSrc.length }),
-      activeIndex: 0,
+      swiper: null,
     }
   },
-  mounted () {
+  mounted() {
     this.init()
   },
-  destroyed () {
+  destroyed() {
     this.destroy()
   },
   computed: {
-    rgbToString () {
-      const rgb = this.colors[this.activeIndex]?.map(c => c * 0.6) || [0, 0, 0]
+    rgbToString() {
+      const defaultColor = [0, 0, 0]
+      if (!this.swiper) {
+        return defaultColor
+      }
+      const rgb =
+        this.colors[this.swiper.activeIndex % this.colors.length]?.map(
+          (c) => c * 0.6
+        ) || defaultColor
       return `rgb(${rgb.join(',')})`
     },
   },
   methods: {
-    loadImage (index) {
-      this.$set(this.colors, index, this.colorThief.getColor((this.$refs.imgs[index])))
+    loadImage(index) {
+      this.$set(
+        this.colors,
+        index,
+        this.colorThief.getColor(this.$refs.imgs[index])
+      )
     },
-    init () {
+    init() {
       if (!this.colorThief) {
         this.colorThief = new ColorThief()
       }
@@ -83,29 +94,44 @@ export default {
         this.swiper = new Swiper(this.$refs.swiperContainer, swiperConfig)
       }
     },
-    destroy () {
+    destroy() {
       this.colorThief = this.swiper = null
     },
-  }
-};
+  },
+}
 </script>
 
 <style lang="sass">
+body
+  background: #eee
+  font-family: Helvetica Neue, Helvetica, Arial, sans-serif
+  font-size: 14px
+  color: #000
+  margin: 0
+  padding: 0
+  box-sizing: border-box
+
 .home
-  height: 100vh
+  height: 80vh
   display: flex
   justify-content: center
   align-items: center
+
 .swiper
   &-container
     width: 750px
+    height: 100%
     padding: 50px 0
   &-wrapper
-    display: flex
-    justify-content: center
     align-items: center
   &-slide
+    background-position: center
+    background-size: cover
     width: 300px
-    > img
+    height: 300px
+    --webkit-box-reflect: below 1px linear-gradient(transparent, transparent, #0006)
+    img
+      object-fit: cover
+      display: block
       width: 100%
 </style>
